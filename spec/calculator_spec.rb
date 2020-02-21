dcb69d8510a76b566f645ba22530a49f8adb8d47
hendersonanalytics <everett@hendersonanalytics.com>
2020-02-20 21:45:18 -0600

    finish red-green testing

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,8 +1,25 @@
 require_relative 'spec_helper'
 
 describe 'Calculator' do
-    it 'instantiates properly' do
-        calculator = Calculator.new
-        expect(calculator).to be_an_instance_of(Calculator)        
-    end    
+    
+    it 'adds properly' do
+        result = Calculator.add(6, 3)
+        expect(result).to eq(9)        
+    end
+    
+    it 'subtracts properly' do
+        result = Calculator.subtract(6, 3)
+        expect(result).to eq(3)        
+    end
+    
+    it 'multiplies properly' do
+        result = Calculator.multiply(6, 3)
+        expect(result).to eq(18)        
+    end
+    
+    it 'divides properly' do
+        result = Calculator.divide(6, 3)
+        expect(result).to eq(2)        
+    end
+    
 end
